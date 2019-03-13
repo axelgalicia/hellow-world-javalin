27abd48ab61ef36ca5ee226f45e2524b04b892c8
Axel Galicia <axelgalicia@gmail.com>
2019-03-13 09:20:47 -0400

    Second commit adding more endpoints

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # hellow-world-javalin
 A Hello World REST API created with Javalin
+
+Currently this end points are available
+
+```
+        app.get("/", ctx -> ctx.result("Hello World"));
+
+        app.get("/hello/:name", ctx -> {
+            ctx.result("Hello: " + ctx.pathParam("name"));
+        });
+
+        app.get("/hello/*/and/*", ctx -> {
+            ctx.result("Hello: " + ctx.splat(0) + " and " + ctx.splat(1));
+        });
+
+```
--- src/main/java/com/arknot/javalin/HelloWorld.java
+++ src/main/java/com/arknot/javalin/HelloWorld.java
@@ -6,5 +6,13 @@ public class HelloWorld {
     public static void main(String[] args) {
         Javalin app = Javalin.create().start(7000);
         app.get("/", ctx -> ctx.result("Hello World"));
+
+        app.get("/hello/:name", ctx -> {
+            ctx.result("Hello: " + ctx.pathParam("name"));
+        });
+
+        app.get("/hello/*/and/*", ctx -> {
+            ctx.result("Hello: " + ctx.splat(0) + " and " + ctx.splat(1));
+        });
     }
 }
