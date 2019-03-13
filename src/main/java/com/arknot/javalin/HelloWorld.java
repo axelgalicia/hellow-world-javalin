d5714ed9bf602c2e057d9fe475ce8b5c9f048022
Axel Galicia <axelgalicia@gmail.com>
2019-03-12 21:59:05 -0400

    First commit

---
+++ src/main/java/com/arknot/javalin/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.arknot.javalin;
+
+import io.javalin.Javalin;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        Javalin app = Javalin.create().start(7000);
+        app.get("/", ctx -> ctx.result("Hello World"));
+    }
+}
